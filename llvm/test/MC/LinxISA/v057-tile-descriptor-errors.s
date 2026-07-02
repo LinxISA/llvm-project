67df7c34553d880ad7a7b0e43f9bbb7381b4fd61
RuoyuZhou <ruoyu.zhou@hisilicon.com>
2026-07-02 21:59:27 +0800

    Revert "Lock v0.57 TileReg descriptor id coverage"
    This reverts commit d53328d59a782a724d1f8b3eb953fa58843fd0d1.

--- llvm/test/MC/LinxISA/v057-tile-descriptor-errors.s
+++
@@ -1,8 +0,0 @@
-# RUN: not llvm-mc -triple=linx64 %s -o /dev/null 2>&1 | FileCheck %s
-
-	.text
-bad_v057_tzero_destination:
-	BSTART.TLOAD FP16
-	B.OTA ->TZERO<0>, last, 0
-
-# CHECK: error: B.OTA destination cannot be TZERO
